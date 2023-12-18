0604997adb038f973daf097aff95cddb4188cb8e
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-12-18 16:54:01 +0530

    Day-1 18-12-2023

---
+++ 2 ifelse.py
@@ -0,0 +1,7 @@
+marks = int(input(" Please Enter Your Subject Marks:  ")) #89
+if marks >= 50:
+    print(" Congratulations ")  # s1
+else:
+    print(" You are Failed")  # s3
+    print(" Better Luck Next Time") #s4
+print("outside if-else")
